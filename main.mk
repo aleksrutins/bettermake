01442c3e28cdca0c058e3e0639b4f5d60e584910
munchkinhalfling <24496030+munchkinhalfling@users.noreply.github.com>
2020-07-22 15:27:00 -0400

    Update main.mk

--- main.mk
+++ main.mk
@@ -3,7 +3,4 @@ bettermake/compile:
 	@$(MAKE) -C bettermake
 bettermake/test: bettermake/compile
 	@echo "Testing BetterMake..."
-	./bettermake/header header test
-	./bettermake/detail 'Test detail'
-	./bettermake/command 'echo hello'
-	./bettermake/command nonexistent-command
+	@$(MAKE) -C bettermake check
