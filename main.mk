587bd0d512496c7dc97529d37999a93d38fa3071
Aleks Rutins <alex@rutins.com>
2021-03-20 17:21:00 -0400

    Cleanup

--- main.mk
+++
@@ -1,6 +0,0 @@
-bettermake/compile:
-	@echo "Compiling BetterMake..."
-	@cd bettermake && ./configure && $(MAKE)
-bettermake/test: bettermake/compile
-	@echo "Testing BetterMake..."
-	@$(MAKE) -C bettermake test
